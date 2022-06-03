a795b17980709542397cdc82808c4d5249ce6896
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-06-02 19:35:28 -0500

    commit tree 2cba0fc41290f5822027a5d78aae799804d7b62f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+      
+    }
+return thankYouCards;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(i) {
+    //for (let i = int; i >= 0; i--)
+    
+    while(i >= 0) {
+        debugger;
+        console.log(i)
+        i = i - 1;
+        debugger;
+    }
+}
+
+countDown(4);
